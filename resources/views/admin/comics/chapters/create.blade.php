dff89c0687c670d92c7edf48b482dcad9466f543
FedericoHeichou <federicosabbatini96@gmail.com>
2020-09-19 17:47:38 +0200

    Miss type

--- resources/views/admin/comics/chapters/create.blade.php
+++ resources/views/admin/comics/chapters/create.blade.php
@@ -2,7 +2,7 @@
 @section('card-title', 'Add new chapter')
 @section('form-action', route('admin.comics.chapters.store', $comic->slug))
 @section('choose-file', 'Choose file')
-@foreach(\App\Models\Comic::getFormFields() as $field)
+@foreach(\App\Models\Chapter::getFormFields() as $field)
     <?php
         if($field['type'] === 'input_checkbox'){
             $default = isset($field['parameters']['checked']) && $field['parameters']['checked'] ? 1 : 0;
