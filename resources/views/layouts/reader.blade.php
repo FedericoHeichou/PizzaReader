e16c3ec837b0ad3bb75cacb5ece8f3b4c7e3fa73
FedericoHeichou <federicosabbatini96@gmail.com>
2021-07-04 09:40:48 +0200

    Added og

--- resources/views/layouts/reader.blade.php
+++ resources/views/layouts/reader.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" prefix="og: https://ogp.me/ns#">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
