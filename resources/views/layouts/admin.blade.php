708a6238faec36f4d47d3b6ee2883cd0010eee74
FedericoHeichou <federicosabbatini96@gmail.com>
2022-01-25 20:06:45 +0100

    Fix console error in admin page

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -26,6 +26,9 @@
     <script src="{{ asset('js/backend.js?ver=0.1.0') }}" defer></script>
     <script src="{{ asset('js/card-search.js') }}" defer></script>
     <script src="{{ asset('js/dark.js') }}" defer></script>
+    <script type="text/javascript">
+        const LOGGED_IN = true;
+    </script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
