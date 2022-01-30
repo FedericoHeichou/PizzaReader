667d917f51665e4a136f78c72b9992e9be7c1cac
FedericoHeichou <federicosabbatini96@gmail.com>
2022-01-30 12:37:23 +0100

    Fix uncorrect namespace

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -19,9 +19,9 @@ ob_start();
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"" . asset('/storage/default.xsl') . "\"?>\n";
 ?>
 <urlset
-    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9/"
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xhtml="http://www.w3.org/1999/xhtml"
-    xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
 >
     <url>
         <loc><![CDATA[{{$base_url}}]]></loc>
